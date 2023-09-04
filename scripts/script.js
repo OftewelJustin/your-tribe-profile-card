088b1e46086905ea0c78b53a0bb1e7e72dc6938a
JustinBerghahn <Justin.berghahn@live.nl>
2023-09-04 22:24:28 +0200

    Yea lot of changed tbh

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,23 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+document.addEventListener('DOMContentLoaded', function () {
+  const expandButton = document.querySelector('.btn-primary');
+  const card = document.querySelector('.card');
+  const content = document.querySelector('.content');
 
-function expand () {
-  document.body.classList.toggle('expand')
-}
+  if (expandButton && card) {
+    expandButton.addEventListener('click', toggleExpand);
+  }
+
+  function toggleExpand() {
+    if (card.classList.contains('expand')) {
+      // If the card is expanded, change it to unexpanded
+      card.classList.remove('expand');
+      card.classList.add('collapse'); // Start collapse animation
+      content.classList.remove('visible');
+    } else {
+      // If the card is unexpanded, change it to expanded
+      card.classList.remove('collapse'); // Remove collapse animation
+      card.classList.add('expand'); // Start expand animation
+      content.classList.add('visible');
+    }
+  }
+});
