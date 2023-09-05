036ca54c4b791e6c84ab2caa56633a99f0b13bb6
JustinBerghahn <Justin.berghahn@live.nl>
2023-09-05 13:36:31 +0200

    #

--- scripts/script.js
+++ scripts/script.js
@@ -13,8 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
       card.classList.remove('expand');
       card.classList.add('collapse'); // Start collapse animation
       content.classList.remove('visible');
+      content.classList.add('invisible'); // Start collapse animation
+
     } else {
       // If the card is unexpanded, change it to expanded
+      content.classList.remove('invisible'); // Remove collapse animation
       card.classList.remove('collapse'); // Remove collapse animation
       card.classList.add('expand'); // Start expand animation
       content.classList.add('visible');
