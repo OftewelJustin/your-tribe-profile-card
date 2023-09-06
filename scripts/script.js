43c275c11d0c397435f1c65d4b738fda43d842a7
JustinBerghahn <Justin.berghahn@live.nl>
2023-09-06 15:49:02 +0200

    #

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const expandButton = document.querySelector('.btn-primary');
+  const expandButton = document.querySelector('.btn-primary' && '.phone');
   const card = document.querySelector('.card');
   const content = document.querySelector('.content');
+  const phone = document.querySelector('.phone');
 
   if (expandButton && card) {
     expandButton.addEventListener('click', toggleExpand);
@@ -14,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
       card.classList.add('collapse'); // Start collapse animation
       content.classList.remove('visible');
       content.classList.add('invisible'); // Start collapse animation
+      phone.classList.remove('rotate');
 
     } else {
       // If the card is unexpanded, change it to expanded
@@ -21,6 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
       card.classList.remove('collapse'); // Remove collapse animation
       card.classList.add('expand'); // Start expand animation
       content.classList.add('visible');
+      phone.classList.add('rotate');
     }
   }
 });
